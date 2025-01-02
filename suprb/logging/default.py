e1129cad4fc5952d2b8a8420ec243433f9742e54
David Pätzel <david.paetzel@posteo.de>
2025-01-02 15:51:08 +0100

    Log elitist genome history in DefaultLogger

--- suprb/logging/default.py
+++ suprb/logging/default.py
@@ -82,6 +82,7 @@ class DefaultLogger(BaseLogger):
         log_metric("elitist_error", elitist.error_)
         log_metric("elitist_complexity", elitist.complexity_)
         log_metric("elitist_matched", matched_training_samples(elitist.subpopulation))
+        log_metric("elitist_genome", elitist.genome)
         # log_metric("elitist_rules", elitist.pool)
 
         # Log performance
